1314e648e569e8b449802553e89f11be945f0637
Pete Jemian <prjemian@gmail.com>
2019-11-20 12:47:58 -0600

    py3.8, too!

--- src/stdlogpj/__init__.py
+++ src/stdlogpj/__init__.py
@@ -43,6 +43,7 @@ __classifiers__ = [
      'Programming Language :: Python',
      'Programming Language :: Python :: 3.6',
      'Programming Language :: Python :: 3.7',
+     'Programming Language :: Python :: 3.8',
      'Topic :: Software Development',
      'Topic :: Utilities',
    ]
